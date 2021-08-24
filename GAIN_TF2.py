c5d2f84349115957f051e614c266b80d40c1516b
minjae <minjae960@snu.ac.kr>
2021-08-24 10:48:19 +0900

    Changed code as added more data

--- GAIN_TF2.py
+++ GAIN_TF2.py
@@ -5,40 +5,56 @@ import pandas as pd
 from tqdm import tqdm
 from scipy import stats
 from sklearn.impute import KNNImputer
-import random
 import time
+import random
 
-data_name = 'pm'
-missing_col = 'ocec'
+data_name = '1_Basic_1_Seoul'
+missing_col = 'ion'
 missing_rate = 0.2
 hint_rate = 0.9
 alpha = 100
-epochs = 3
-sq = 48
+epochs = 1
+sq = 360
+lr = 1e-4
 
 # 1. data preparation
 # 1-1) load dataset, data
-data = pd.read_csv('data/{}.csv'.format(data_name))
+if data_name == 'pm' or data_name == 'pm_wthr':
+    data = pd.read_csv('data/{}.csv'.format(data_name))
+else:
+    data = pd.read_csv('data/{}_raw.csv'.format(data_name)).drop(columns='date')
+
 rows, cols = data.shape
 
 # 1-2) binary data, data_m
-def missing_sampler(rate, rows, cols):
+def missing_sampler(rate, rows, cols, type):
     unif_matrix = np.full((rows, cols), 1)
 
-    num_rows = int(rows*rate)
+    if data_name == 'pm' or data_name == 'pm_wthr':
+        col_dic = {'ocec': ['OC', 'EC'],
+                   'ele': ['Si', 'S', 'K', 'Ca', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Ni', 'Cu', 'Zn', 'As', 'Se', 'Br', 'Ba', 'Pb'],
+                   'ion': ['SO42.', 'NO3.', 'Cl.', 'Na.', 'NH4.', 'K.', 'Mg2.', 'Ca2.'],
+                   'ocec+ele': ['OC', 'EC', 'Si', 'S', 'K', 'Ca', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Ni', 'Cu', 'Zn', 'As', 'Se', 'Br', 'Ba', 'Pb'],
+                   'ocec+ion': ['OC', 'EC', 'SO42.', 'NO3.', 'Cl.', 'Na.', 'NH4.', 'K.', 'Mg2.', 'Ca2.'],
+                   'ele+ion': ['Si', 'S', 'K', 'Ca', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Ni', 'Cu', 'Zn', 'As', 'Se', 'Br', 'Ba', 'Pb', 'SO42.', 'NO3.', 'Cl.', 'Na.', 'NH4.', 'K.', 'Mg2.', 'Ca2.'],
+                   'all': ['OC', 'EC', 'Si', 'S', 'K', 'Ca', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Ni', 'Cu', 'Zn', 'As', 'Se', 'Br', 'Ba', 'Pb', 'SO42.', 'NO3.', 'Cl.', 'Na.', 'NH4.', 'K.', 'Mg2.', 'Ca2.']
+                   }
+    else:
+        col_dic = {'ocec': ['OC', 'EC'],
+                   'ele': ['S', 'K', 'Ca', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Ni', 'Cu', 'Zn', 'As', 'Se', 'Br', 'Pb'],
+                   'ion': ['SO42-', 'NO3-', 'Cl-', 'Na+', 'NH4+', 'K+', 'Mg2+', 'Ca2+'],
+                   'ocec+ele': ['OC', 'EC', 'S', 'K', 'Ca', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Ni', 'Cu', 'Zn', 'As', 'Se', 'Br', 'Pb'],
+                   'ocec+ion': ['OC', 'EC', 'SO42-', 'NO3-', 'Cl-', 'Na+', 'NH4+', 'K+', 'Mg2+', 'Ca2+'],
+                   'ele+ion': ['S', 'K', 'Ca', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Ni', 'Cu', 'Zn', 'As', 'Se', 'Br', 'Pb', 'SO42-', 'NO3-', 'Cl-', 'Na+', 'NH4+', 'K+', 'Mg2+', 'Ca2+'],
+                   'all': ['OC', 'EC', 'SO42-', 'NO3-', 'Cl-', 'Na+', 'NH4+', 'K+', 'Mg2+', 'Ca2+', 'S', 'K', 'Ca', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Ni', 'Cu', 'Zn', 'As', 'Se', 'Br', 'Pb']
+                   }
 
-    col_dic = {'ocec': ['OC', 'EC'],
-             'ele': ['Si', 'S', 'K', 'Ca', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Ni', 'Cu', 'Zn', 'As', 'Se', 'Br', 'Ba', 'Pb'],
-             'ion': ['SO42.', 'NO3.', 'Cl.', 'Na.', 'NH4.', 'K.', 'Mg2.', 'Ca2.'],
-             'ocec+ele': ['OC', 'EC', 'Si', 'S', 'K', 'Ca', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Ni', 'Cu', 'Zn', 'As', 'Se', 'Br', 'Ba', 'Pb'],
-             'ocec+ion': ['OC', 'EC', 'SO42.', 'NO3.', 'Cl.', 'Na.', 'NH4.', 'K.', 'Mg2.', 'Ca2.'],
-             'ele+ion': ['SO42.', 'NO3.', 'Cl.', 'Na.', 'NH4.', 'K.', 'Mg2.', 'Ca2.', 'Si', 'S', 'K', 'Ca', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Ni', 'Cu', 'Zn', 'As', 'Se', 'Br', 'Ba', 'Pb'],
-             'all': ['OC', 'EC', 'SO42.', 'NO3.', 'Cl.', 'Na.', 'NH4.', 'K.', 'Mg2.', 'Ca2.', 'Si', 'S', 'K', 'Ca', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Ni', 'Cu', 'Zn', 'As', 'Se', 'Br', 'Ba', 'Pb']
-             }
+    if type == 'data_m':
+        random_row = data.sample(int(len(data) * rate), random_state=777).index.tolist()
+    elif type == 'hint':
+        missing_num = int(rows * rate)
+        random_row = random.sample(range(rows), missing_num)
 
-    random.seed(777)
-
-    random_row = random.sample(range(rows), num_rows)
     data_col = data.columns.tolist()
 
     col_index = []
@@ -50,20 +66,20 @@ def missing_sampler(rate, rows, cols):
 
     return unif_matrix
 
-data_m = missing_sampler(missing_rate, rows, cols)
+data_m = missing_sampler(missing_rate, rows, cols, 'data_m')
 
 # 1-3) data_x and miss_data_x
-data_x = data.to_numpy()
-miss_data_x = data_x.copy()
-miss_data_x[data_m==0] = np.nan
+data_ori_x = data.to_numpy()
+data_miss_x = data_ori_x.copy()
+data_miss_x[data_m==0] = np.nan
 
-# 1-4) normalize miss_data_x
-min_vector = np.nanmin(miss_data_x, axis=0)
-max_vector = np.nanmax(miss_data_x, axis=0)
+# 1-4) normalize data_miss_x
+min_vector = np.nanmin(data_miss_x, axis=0)
+max_vector = np.nanmax(data_miss_x, axis=0)
 
-knn_norm_x = (miss_data_x - min_vector)/(max_vector - min_vector)
-miss_norm_x = knn_norm_x.copy()
-miss_norm_x = np.nan_to_num(miss_norm_x, 0)
+data_norm_x = (data_miss_x - min_vector)/(max_vector - min_vector)
+train_x = data_norm_x.copy()
+train_x = np.nan_to_num(train_x, 0)
 
 
 # 2. GAIN
@@ -71,7 +87,7 @@ miss_norm_x = np.nan_to_num(miss_norm_x, 0)
 def make_generator_model():
     model = tf.keras.Sequential()
     model.add(layers.LSTM(cols, kernel_initializer='glorot_normal', input_shape=(None, cols*2), return_sequences=True))
-    model.add(layers.LSTM(cols, kernel_initializer='glorot_normal', return_sequences=True))
+    # model.add(layers.LSTM(cols, kernel_initializer='glorot_normal', return_sequences=True))
     model.add(layers.Dense(cols, activation='sigmoid', kernel_initializer = 'glorot_normal'))
 
     return model
@@ -79,7 +95,7 @@ def make_generator_model():
 def make_discriminator_model():
     model = tf.keras.Sequential()
     model.add(layers.LSTM(cols, kernel_initializer='glorot_normal', input_shape=(sq, cols*2), return_sequences=True))
-    model.add(layers.LSTM(cols, kernel_initializer='glorot_normal', return_sequences=True))
+    # model.add(layers.LSTM(cols, kernel_initializer='glorot_normal', return_sequences=True))
     model.add(layers.Dense(cols, activation='sigmoid', kernel_initializer='glorot_normal'))
 
     return model
@@ -99,8 +115,8 @@ def G_loss(batch_x, batch_m, G_output, D_output):
     return total_loss
 
 # 2-3) optimizer
-generator_optimizer = tf.keras.optimizers.Adam(1e-4)
-discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+generator_optimizer = tf.keras.optimizers.Adam(lr)
+discriminator_optimizer = tf.keras.optimizers.Adam(lr)
 
 # 2-4) train function
 generator = make_generator_model()
@@ -108,7 +124,7 @@ discriminator = make_discriminator_model()
 
 def train_step(batch_x, batch_m):
     Z_mb = np.random.uniform(0, 0.01, size = [1, sq, cols])
-    H_mb = batch_m * missing_sampler(1-hint_rate, sq, cols)
+    H_mb = batch_m * missing_sampler(1-hint_rate, sq, cols, 'hint')
     X_mb = batch_m * batch_x + (1-batch_m) * Z_mb
 
     G_input = tf.concat(values = [X_mb, batch_m], axis = 2)
@@ -137,7 +153,7 @@ def train_lstm_gain(epochs): # inputting (1, sq, cols) size data with random ord
     batch_m_sq = []
 
     for i in range(rows-sq+1):
-        x = miss_norm_x[i:i+sq, :]
+        x = train_x[i:i+sq, :]
         m = data_m[i:i+sq, :]
 
         batch_x_sq.append(x)
@@ -171,30 +187,33 @@ train_lstm_gain(epochs)
 # 3. Output (GAIN, KNN)
 # 3-1) GAIN imputed data
 test_z = np.random.uniform(0, 0.01, size = [rows, cols])
-test_x = data_m * miss_norm_x + (1-data_m) * test_z
+test_x = data_m * train_x + (1-data_m) * test_z
 test_x = np.concatenate((test_x, data_m), axis=1)
 
 test_x_sq = test_x.reshape(-1, rows, cols*2)
 
-GAIN_imputed = generator(test_x_sq, training=False)
+GAIN_imputed = train_x * data_m + generator(test_x_sq, training=False)[0] * (1-data_m)
+GAIN_imputed = np.array(GAIN_imputed)
 
 # 3-2) KNN imputed data
 imputer = KNNImputer(n_neighbors = 3)
-KNN_imputed = imputer.fit_transform(knn_norm_x)
+KNN_imputed = imputer.fit_transform(data_norm_x)
 
 # 3-3) denormalization
-GAIN_imputed = GAIN_imputed[0]
 GAIN_imputed = GAIN_imputed * (max_vector - min_vector) + min_vector
 KNN_imputed = KNN_imputed * (max_vector - min_vector) + min_vector
 
-# 3-3) r-square value
-REAL = data_x[data_m == 0]
-GAIN = GAIN_imputed[data_m == 0]
-KNN = KNN_imputed[data_m == 0]
+# 3-4) r-square value
+def r_square():
+    REAL = data_ori_x[data_m == 0]
+    GAIN = GAIN_imputed[data_m == 0]
+    KNN = KNN_imputed[data_m == 0]
 
-slope_GAIN, intercept_GAIN, r_value_GAIN, p_value_GAIN, std_err_GAIN = stats.linregress(REAL, GAIN)
-slope_KNN, intercept_KNN, r_value_KNN, p_value_KNN, std_err_KNN = stats.linregress(REAL, KNN)
+    slope_GAIN, intercept_GAIN, r_value_GAIN, p_value_GAIN, std_err_GAIN = stats.linregress(REAL, GAIN)
+    slope_KNN, intercept_KNN, r_value_KNN, p_value_KNN, std_err_KNN = stats.linregress(REAL, KNN)
 
-print()
-print('GAIN r-square value is', round(r_value_GAIN ** 2, 4))
-print('KNN r-square value is', round(r_value_KNN ** 2, 4))
+    print()
+    print('GAIN r-square value is', round(r_value_GAIN ** 2, 4))
+    print('KNN r-square value is', round(r_value_KNN ** 2, 4))
+
+r_square()
